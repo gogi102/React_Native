4429eeba2ad0a979f5899f8645a048a0c981b38b
gogi102 <yks071210@naver.com>
2024-10-14 19:25:32 +0900

    commit

--- my-first-rn/src/screen/SignInScreen.js
+++ my-first-rn/src/screen/SignInScreen.js
@@ -1,57 +1,59 @@
 import { Image, StyleSheet, View, Keyboard, Alert } from 'react-native';
-import Input, {
-  KeyboardTypes,
-  ReturnKeyTypes,
-  IconNames,
-} from '../components/Input';
+import Input, { KeyboardTypes, ReturnKeyTypes, IconNames } from '../components/Input';
 import { useState, useRef, useEffect } from 'react';
 import Button from '../components/Button';
 import { signIn } from '../api/auth';
+import PropTypes from 'prop-types';
 
-const SignInScreen = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const passwordRef = useRef(null);
-  const [disabled, setDisabled] = useState(true);
-  const [isLoading, setIsLoading] = useState(false);
+const SignInScreen = ({ navigation }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const passwordRef = useRef(null);
+    const [disabled, setDisabled] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
+    
 
-  useEffect(() => {
-    setDisabled(!email || !password);
-  }, [email, password]);
+    useEffect(() => {
+      setDisabled(!email || !password);
+    }, [email, password]);
 
-  const onSubmit = async () => {
-    if (!isLoading && !disabled) {
-      try {
-        Keyboard.dismiss();
-        const data = await signIn(email, password);
-        console.log(data);
+    const onSubmit = async () => {
+      if(!isLoading && !disabled){
+        try {
+          Keyboard.dismiss();
+          const data = await signIn(email, password);
+          console.log(data);
+          setIsLoading(false);
+          navigation.navigate('List');
+        } catch (error) {
+          Alert.alert('로그인 실패', error, [
+            { text: '확인', onPress: () => setIsLoading(false) },
+          ]);
+        }
         setIsLoading(false);
-      } catch (error) {
-        Alert.alert('로그인 실패', error, [
-          { text: '확인', onPress: () => setIsLoading(false) },
-        ]);
-      }
-      setIsLoading(false);
-    }
-  };
+      } 
+    };
 
   return (
     <View style={styles.container}>
-      <Image source={require('../../assets/main.png')} style={styles.image} />
-      <Input
+      <Image 
+        source={require('../../assets/main.png')}
+        style={styles.image}
+      />
+      <Input 
         title={'이메일'}
         placeholder="myemail@test.com"
-        keyboardType={KeyboardTypes.EMAIL}
-        returnKeyType={ReturnKeyTypes.NEXT}
+        keyboardType={KeyboardTypes.EMAIL}  
+        returnKeyType={ReturnKeyTypes.NEXT} 
         value={email}
         onChangeText={(email) => setEmail(email.trim())}
         iconName={IconNames.EMAIL}
         onSubmitEditing={() => passwordRef.current.focus()}
       />
-      <Input
+      <Input 
         ref={passwordRef}
-        title={'비밀번호'}
-        returnKeyType={ReturnKeyTypes.DONE}
+        title={'비밀번호'} 
+        returnKeyType={ReturnKeyTypes.DONE}  
         secureTextEntry
         value={password}
         onChangeText={(password) => setPassword(password.trim())}
@@ -59,15 +61,18 @@ const SignInScreen = () => {
         onSubmitEditing={onSubmit}
       />
       <View style={styles.buttonContainer}>
-        <Button
-          title="로그인"
-          onPress={onSubmit}
-          disabled={disabled}
-          isLoading={isLoading}
-        />
+        <Button title="로그인" 
+                onPress={onSubmit} 
+                disabled={disabled}
+                isLoading={isLoading}
+              />
       </View>
     </View>
   );
+};
+
+SignInScreen.propTypes = {
+  navigation: PropTypes.object,
 };
 
 const styles = StyleSheet.create({
@@ -87,5 +92,6 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
   },
 });
+
 
 export default SignInScreen;
